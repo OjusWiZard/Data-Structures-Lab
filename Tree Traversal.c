30853fcaae170ba4c625f298306998a2ccc94878
OjusWiZard <ojuswimail@gmail.com>
2020-10-08 13:24:03 +0530

    Tree Traversal

---
+++ Tree Traversal.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *left, *right;
+};
+
+struct node *insert(struct node *root, int data, char *path)
+{
+	if (root == NULL)
+	{
+		struct node *new = (struct node *)malloc(sizeof(struct node));
+		new->data = data;
+		new->left = new->right = NULL;
+		return new;
+	}
+	else if (path[0] == 'L')
+		root->left = insert(root->left, data, &path[1]);
+	else
+		root->right = insert(root->right, data, &path[1]);
+	return root;
+}
+
+struct node *delete (struct node *root, char *path)
+{
+	if (root->left == NULL && root->right == NULL)
+	{
+		free(root);
+		root = NULL;
+	}
+	else if (path[0] == 'L')
+		root->left = delete (root->left, &path[1]);
+	else
+		root->right = delete (root->right, &path[1]);
+	return root;
+}
+
+void displayPreOrder(struct node *root)
+{
+	printf("%d ", root->data);
+	if (root->left != NULL)
+		displayPreOrder(root->left);
+	if (root->right != NULL)
+		displayPreOrder(root->right);
+}
+
+void displayInOrder(struct node *root)
+{
+	if (root->left != NULL)
+		displayInOrder(root->left);
+	printf("%d ", root->data);
+	if (root->right != NULL)
+		displayInOrder(root->right);
+}
+
+void displayPostOrder(struct node *root)
+{
+	if (root->left != NULL)
+		displayPostOrder(root->left);
+	if (root->right != NULL)
+		displayPostOrder(root->right);
+	printf("%d ", root->data);
+}
+
+int main()
+{
+	struct node *root = NULL;
+	printf("-1 to Exit\n1 <element> <path with L&R> to insert\n2 <element> <path with L&R> to delete\n\n");
+	while (1)
+	{
+		int choice;
+		scanf("%d", &choice);
+		if (choice == -1)
+			break;
+
+		if (choice == 1)
+		{
+			int e;
+			scanf("%d", &e);
+			char path[10] = {'\0'};
+			if (root != NULL)
+				scanf("%s", path);
+			root = insert(root, e, path);
+		}
+		else if (choice == 2)
+		{
+			char path[10];
+			scanf("%s", path);
+			root = delete (root, path);
+		}
+		printf("Current Tree (preOrder): ");
+		displayPreOrder(root);
+		printf("\nCurrent Tree (inOrder): ");
+		displayInOrder(root);
+		printf("\nCurrent Tree (postOrder): ");
+		displayPostOrder(root);
+		printf("\n\n");
+	}
+
+	printf("\n\n\n");
+	system("pause");
+	return 0;
+}
